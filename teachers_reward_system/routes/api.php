9a9c5283fd742449c8ff8e371df29e6137da96f9
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2025-12-24 17:10:20 +0300

    api fixed

--- teachers_reward_system/routes/api.php
+++ teachers_reward_system/routes/api.php
@@ -59,7 +59,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/schooladmin/dashboard', [SchoolAdminDashboardController::class, 'index']);
     });
     Route::middleware(CheckRole::class . ':teacher')->group(function () {
-        Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index']);
+        Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'dashboard']);
         Route::get('/teacher/ratings', [TeacherRatingRewardController::class, 'ratings']);
         Route::get('/teacher/rewards', [TeacherRatingRewardController::class, 'rewards']);
         Route::post('/teacher/withdraw', [TeacherRatingRewardController::class, 'requestWithdrawal']);
